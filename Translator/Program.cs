5afb1ac593c1c0f1f91aee6e24c0def70f1b7c94
kwekuhammond <hammondkweku@gmail.com>
2021-12-10 17:25:19 +0000

    uploads project

---
+++ Translator/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MyTranslator;
+
+Console.WriteLine("Enter text to be translated . . .");
+string Content = Console.ReadLine();
+
+Console.WriteLine("Enter language code . . .");
+string LangCode = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(Content) || string.IsNullOrWhiteSpace(LangCode))
+    return;
+
+//if(GTranslate.Language.GetLanguage($"{LangCode}").IsServiceSupported)
+
+var Output = await TranslatorClass.TranslateAsync(Content, LangCode.ToLower());
+
+Console.WriteLine($"Translated Text: {Output}");
---
+++ Translator/TranslatorClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using GTranslate.Translators;
+
+namespace MyTranslator
+{
+    internal class TranslatorClass
+    {
+        public static async Task<string> TranslateAsync(string TextToBeTranslated, string LangCode)
+        {
+            var GTranslator = new Translator();
+            string translatedText = string.Empty;
+            try
+            {
+                    var translationResult = await GTranslator.TranslateAsync(TextToBeTranslated, GTranslate.Language.GetLanguage($"{LangCode}"));
+                    translatedText = translationResult.Result;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return translatedText;
+        }
+    }
+}
